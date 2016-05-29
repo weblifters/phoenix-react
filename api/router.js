106312b7d77eba9fdb1465cbf3cf3d9dc06be3c9
Ben Nelson <benn@ifit.com>
2016-05-28 21:12:04 -0600

    Implemented authentication on the front end

--- api/router.js
+++ api/router.js
@@ -8,7 +8,7 @@ const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
   app.get('/', requireAuth, function(req, res) {
-    res.send({ hi: 'there' });
+    res.send({ message: 'super secret code' });
   });
   app.post('/signin',
     requireSignin,
