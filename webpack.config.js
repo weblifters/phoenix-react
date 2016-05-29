35b02783e5b2f5ab2a6a945953ad7a74dfe2f18f
Ben Nelson <benn@ifit.com>
2016-05-28 21:15:03 -0600

    Cleaned up webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -21,32 +21,3 @@ module.exports = {
     contentBase: './'
   }
 };
-//var webpack = require('webpack');
-//module.exports = {
-//  entry: [
-//    'webpack-dev-server/client?http://' + require("os").hostname() + ':3000/',
-////    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
-//    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
-//    './src/index.js'
-//  ],
-//  output: {
-//    path: __dirname,
-//    publicPath: '/',
-//    filename: 'bundle.js'
-//  },
-//  module: {
-//    loaders: [{
-//      exclude: /node_modules/,
-//      loaders: ['react-hot', 'babel']
-//    }]
-//  },
-//  resolve: {
-//    extensions: ['', '.js', '.jsx']
-//  },
-//  devServer: {
-//    contentBase: './'
-//  },
-//  plugins: [
-//    new webpack.HotModuleReplacementPlugin()
-//  ]
-//};
